d7118af69b146c2ba3ae34dd30cdc0543013e6a0
palmtom <palmtom@localhost>
2025-12-10 15:37:07 +0800

    Add /uploads proxy to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,6 +24,11 @@ export default defineConfig({
                 target: 'http://127.0.0.1:8000',
                 changeOrigin: true,
                 secure: false
+            },
+            '/uploads': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                secure: false
             }
         }
     }
